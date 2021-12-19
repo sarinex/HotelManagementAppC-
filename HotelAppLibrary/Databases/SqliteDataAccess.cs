58f76cdeca5b2b5ed6daadea3c2f8b3615e292df
Sarinex <razvanstefanin@gmail.com>
2021-12-19 16:22:18 +0200

    Code Added

---
+++ HotelAppLibrary/Databases/SqliteDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+using Dapper;
+using System.Linq;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqliteDataAccess : ISqliteDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqliteDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement,
+                                      U parameters,
+                                      string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+                                T parameters,
+                                string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
